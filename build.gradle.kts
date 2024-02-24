85249355768d8a3dfa090238d22db1577b9aa842
Kélvin Tesche Ievenes <kelvintesche@gmail.com>
2024-02-24 18:58:43 -0300

    feat: adding all ktlint references for the gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,34 @@
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.KtlintPlugin
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+import org.jlleitschuh.gradle.ktlint.tasks.GenerateReportsTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+    alias(libs.plugins.ktlint) apply false
 }
-true // Needed to make the Suppress annotation work for the plugins block
+
+allprojects {
+    plugins.apply(KtlintPlugin::class.java)
+
+    configure<KtlintExtension> {
+        android.set(true)
+        verbose.set(true)
+        reporters {
+            reporter(ReporterType.PLAIN)
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.HTML)
+        }
+        filter {
+            include("**/*.kt")
+            exclude("**/build/**")
+        }
+    }
+
+    tasks.withType<GenerateReportsTask> {
+        reportsOutputDirectory.set(layout.buildDirectory.dir("reports/ktlint/"))
+    }
+}
