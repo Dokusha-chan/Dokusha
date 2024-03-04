17a099b7b44b3a9bfbd5b89334b0d8b0cf5c4bcb
Kélvin Tesche Ievenes <kelvintesche@gmail.com>
2024-03-04 18:45:29 -0300

    feat: adding submodules, hilt, kapt and toml improved

---
+++ data/src/main/kotlin/app/dokusha/data/DataApplication.kt
@@ -0,0 +1,11 @@
+package app.dokusha.data
+
+import android.app.Application
+
+class DataApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ domain/src/main/kotlin/app/dokusha/domain/DomainApplication.kt
@@ -0,0 +1,10 @@
+package app.dokusha.domain
+
+import android.app.Application
+
+class DomainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
